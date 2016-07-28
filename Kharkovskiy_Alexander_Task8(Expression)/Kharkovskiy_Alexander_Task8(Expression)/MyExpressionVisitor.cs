1543282d17c9ad17fe91e9bead9385a8e29b5087
Александр <sckorpions@yandex.ru>
2016-07-29 00:52:01 +0400

    fix

---
+++ Kharkovskiy_Alexander_Task8(Expression)/Kharkovskiy_Alexander_Task8(Expression)/MyExpressionVisitor.cs
@@ -0,0 +1,34 @@
+﻿using System.Linq.Expressions;
+
+namespace Kharkovskiy_Alexander_Task8_Expression_
+{
+    class MyExpressionVisitor: ExpressionVisitor
+    {
+        public Expression Modify(Expression expression)
+        {
+            return Visit(expression);
+        }
+
+        protected override Expression VisitBinary(BinaryExpression b)
+        {
+            if (b.NodeType == ExpressionType.Add)
+            {
+                var left = Visit(b.Left);
+                var right = Visit(b.Right);
+                {
+                    int temp;
+                    if (int.TryParse(right.ToString(), out temp) && temp == 1)
+                    { 
+                        return Expression.MakeUnary(ExpressionType.Increment, left, b.Type, b.Method);
+                    }
+                    if (int.TryParse(left.ToString(), out temp) && temp == 1)
+                    {
+                        return Expression.MakeUnary(ExpressionType.Increment, right, b.Type, b.Method);
+                    }
+                }
+            }
+
+            return base.VisitBinary(b);
+        }
+    }
+}
---
+++ Kharkovskiy_Alexander_Task8(Expression)/Kharkovskiy_Alexander_Task8(Expression)/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq.Expressions;
+
+
+namespace Kharkovskiy_Alexander_Task8_Expression_
+{
+    class Program
+    {
+        static void Main()
+        {
+            Expression<Func<int, int>> expr = a => a + 1;
+            Console.WriteLine($"{expr} при a = 5 дал результат: {expr.Compile().Invoke(5)}");
+
+            var treeModifier = new MyExpressionVisitor();
+            var modifiedExpr = treeModifier.Modify(expr);
+
+            Console.WriteLine($"{modifiedExpr} при a = 5 дал результат: {expr.Compile().Invoke(5)}");
+
+            Expression<Func<int, int>> expr2 = b => 1 + b;
+            Console.WriteLine($"{expr2} при b = 5 дал результат: {expr2.Compile().Invoke(5)}");
+            var modifiedExpr2 = treeModifier.Modify(expr2);
+            Console.WriteLine($"{modifiedExpr2} при b = 5 дал результат: {expr2.Compile().Invoke(5)}");
+
+            // Формула для подсчета среднего двух чисел:
+            Expression<Func<int, int, double>> expr3 = (a, b) => (a + b)/2.0;
+            Console.WriteLine($"{expr3} при a = 5 и b = 6 дал результат: {expr3.Compile().Invoke(5,6)}");
+           
+            Console.Read();
+        }
+    }
+}
